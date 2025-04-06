a5d7dade752ed42781458fcc7ee349a03d7ba971
Uzeyir <uzeyir@example.com>
2025-04-06 21:59:09 +0400

    develop file

--- src/layout/header/headerResponsive/HeaderResponsive.tsx
+++ src/layout/header/headerResponsive/HeaderResponsive.tsx
@@ -8,19 +8,25 @@ import { RxAvatar } from "react-icons/rx";
 import { FaFacebookF } from "react-icons/fa6";
 import { BsTwitterX } from "react-icons/bs";
 import { BiLogoInstagramAlt } from "react-icons/bi";
-import { MdKeyboardArrowRight } from "react-icons/md";
+import { MdKeyboardArrowRight, MdOutlineKeyboardArrowLeft } from "react-icons/md";
 import { useNavigate } from "react-router-dom";
 
 const HeaderResponsive = () => {
   const [isMenuOpen, setIsMenuOpen] = useState(false);
   const [isSticky, setIsSticky] = useState(false);
-  const [isInfoOpen, setIsInfoOpen] = useState(false);
+  const [isInfoVisible, setIsInfoVisible] = useState(false);
   const navigate = useNavigate();
+
+  const toggleInfo = (event: React.MouseEvent) => {
+    event.stopPropagation();
+    setIsInfoVisible(prev => !prev);
+  };
 
   const handleNavigation = (path: string) => {
     navigate(path);
     window.scrollTo(0, 0);
-    setIsMenuOpen(false); // Menü kapansın
+    setIsMenuOpen(false); 
+    setIsInfoVisible(false); 
   };
 
   useEffect(() => {
@@ -34,10 +40,7 @@ const HeaderResponsive = () => {
 
   const toggleMenu = () => {
     setIsMenuOpen(!isMenuOpen);
-  };
-
-  const toggleInfoMenu = () => {
-    setIsInfoOpen(!isInfoOpen);
+    setIsInfoVisible(false); 
   };
 
   useEffect(() => {
@@ -45,6 +48,7 @@ const HeaderResponsive = () => {
       const target = event.target as HTMLElement | null;
       if (
         isMenuOpen &&
+        !isInfoVisible &&
         target &&
         !target.closest(`.${style.sidebar}`) &&
         !target.closest(`.${style.header_barIcon}`)
@@ -57,7 +61,7 @@ const HeaderResponsive = () => {
     return () => {
       document.removeEventListener("click", handleOutsideClick);
     };
-  }, [isMenuOpen]);
+  }, [isMenuOpen, isInfoVisible]);
 
   return (
     <div className={style.header_mobile_container}>
@@ -67,10 +71,18 @@ const HeaderResponsive = () => {
             src="https://greenshift-road.myshopify.com/cdn/shop/files/GlideX_Logo.svg?v=1709810815&width=263"
             alt="Logo"
             className={style.header_logo}
+            onClick={() => handleNavigation("/")}
+            style={{ cursor: "pointer" }}
           />
           <div className={style.header_mobile_right}>
-            <CiSearch className={style.header_right_icon} />
-            <MdOutlineShoppingBag className={style.header_right_icon} />
+            <CiSearch
+              className={style.header_right_icon}
+              onClick={() => handleNavigation("/search")}
+            />
+            <MdOutlineShoppingBag
+              className={style.header_right_icon}
+              onClick={() => handleNavigation("/cart")}
+            />
             <FaBarsStaggered
               className={style.header_barIcon}
               onClick={toggleMenu}
@@ -78,42 +90,58 @@ const HeaderResponsive = () => {
           </div>
         </div>
       </div>
-      <div className={`${style.sidebar} ${isMenuOpen ? style.open : ""}`}>
-        <div className={style.sideBar_head}>
-          <button className={style.close_button} onClick={toggleMenu}>
-            <IoClose className={style.sideBar_head_closeIcon} />
-          </button>
-          <ul>
-            <li className={style.sideBar_head_li} onClick={() => handleNavigation("/")}>Home</li>
-            <li className={style.sideBar_head_li}>
-              Shop <MdKeyboardArrowRight className={style.sideBar_head_li_icon} />
-            </li>
-            <li className={style.sideBar_head_li} onClick={() => handleNavigation("/about")}>About</li>
-            <li className={style.sideBar_head_li} onClick={() => handleNavigation("/Courses")}>Courses</li>
-            <li className={style.sideBar_head_li} onClick={() => handleNavigation("/contact")}>Contact</li>
 
-            {/* Information Menü */}
-            <li className={style.sideBar_head_li} onClick={toggleInfoMenu}>
-              Information 
-              <MdKeyboardArrowRight className={style.sideBar_head_li_icon} />
-            </li>
-            {/* Açılır Menü İçeriği */}
-            <ul className={`${style.sideBar_subMenu} ${isInfoOpen ? style.open : ""}`}>
-              <li className={style.sideBar_subItem} onClick={() => handleNavigation("/event")}>Event</li>
-              <li className={style.sideBar_subItem} onClick={() => handleNavigation("/instructor")}>Instructor</li>
-              <li className={style.sideBar_subItem} onClick={() => handleNavigation("/blog")}>Blog</li>
+      <div className={isMenuOpen ? `${style.sidebar} ${style.open}` : style.sidebar}>
+        {!isInfoVisible && (
+          <div className={style.sideBar_head}>
+            <button className={style.close_button} onClick={toggleMenu}>
+              <IoClose className={style.sideBar_head_closeIcon} />
+            </button>
+            <ul className={style.sideBar_head_ul}>
+              <li className={style.sideBar_head_li} onClick={() => handleNavigation("/")}>Home</li>
+              <li className={style.sideBar_head_li} onClick={() => handleNavigation("/about")}>About</li>
+              <li className={style.sideBar_head_li} onClick={() => handleNavigation("/Courses")}>Courses</li>
+              <li className={style.sideBar_head_li} onClick={() => handleNavigation("/contact")}>Contact</li>
+              <li className={style.sideBar_head_li} onClick={toggleInfo}>
+                Information <MdKeyboardArrowRight className={style.sideBar_head_li_icon} />
+              </li>
             </ul>
-          </ul>
-        </div>
+          </div>
+        )}
+
+        {isInfoVisible && (
+          <div className={style.sideBar_head_Information}>
+            <button className={style.close_button} onClick={toggleInfo}>
+              <MdOutlineKeyboardArrowLeft className={style.sideBar_head_closeIcon} />
+              <h3>Information</h3>
+            </button>
+            <ul>
+              <li className={style.sideBar_head_li} onClick={() => handleNavigation("/event")}>Event</li>
+              <li className={style.sideBar_head_li} onClick={() => handleNavigation("/Instructor")}>Instructor</li>
+              <li className={style.sideBar_head_li} onClick={() => handleNavigation("/blog")}>Blog</li>
+            </ul>
+          </div>
+        )}
+
         <div className={style.sideBar_bottom}>
-          <div className={style.sideBar_login}>
+          <div
+            className={style.sideBar_login}
+            onClick={() => handleNavigation("/login")}
+            style={{ cursor: "pointer" }}
+          >
             <RxAvatar className={style.sideBar_login_icon} />
             <p>Login</p>
           </div>
           <div className={style.sidebar_social}>
-            <BsTwitterX className={style.sidebar_social_icon} />
-            <FaFacebookF className={style.sidebar_social_icon} />
-            <BiLogoInstagramAlt className={style.sidebar_social_icon} />
+            <a href="https://twitter.com" target="_blank" rel="noopener noreferrer">
+              <BsTwitterX className={style.sidebar_social_icon} />
+            </a>
+            <a href="https://facebook.com" target="_blank" rel="noopener noreferrer">
+              <FaFacebookF className={style.sidebar_social_icon} />
+            </a>
+            <a href="https://instagram.com" target="_blank" rel="noopener noreferrer">
+              <BiLogoInstagramAlt className={style.sidebar_social_icon} />
+            </a>
           </div>
         </div>
       </div>
